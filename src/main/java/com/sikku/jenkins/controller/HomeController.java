d51436df26042e3e907d9d0f5b1c68e4b9951621
Upendra <upendrasatna@gmail.com>
2024-01-25 18:08:28 +0530

    first commit

---
+++ src/main/java/com/sikku/jenkins/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sikku.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String showHome() {
+		return "welcome to jenkins";
+	}
+
+}
